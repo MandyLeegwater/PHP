9222c0affb1bbb93c7b9a3a87580c735aee518f2
m-lee <m-leegwater@hotmail.com>
2018-02-15 09:49:00 +0100

    PHP beginnersCursus / De Busreis

---
+++ PHP BeginnersCursus/Busreis.php
@@ -0,0 +1,19 @@
+<?php
+
+$leeftijd = 12;
+
+
+if ($leeftijd > 65){
+    echo "De reis kost 5 euro";
+}
+else if ($leeftijd <3){
+    echo "De reis kost 0 euro";
+}
+else if ($leeftijd <= 12){
+    echo "De reis kost 5 euro";
+}
+else {
+    echo "De reis kost 10 euro";
+}
+
+?>
